20a050caba772802b109a49ce6e01fd379c2063e
Tim Carlson <writetimcarlson@gmail.com>
2019-01-20 23:44:58 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -35,7 +35,6 @@ def using_first(array)
       it "takes in argument of an array and and returns the first element in the array" do
         array = ["wow", "I", "am", "really", "learning", "arrays!"]
         expect(using_first(array)).to eq("wow")
-
 end
 
 def using_last(array)
